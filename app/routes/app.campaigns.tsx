4e17d874c9bbc538ad748188bbb46f7a33d825ca
johnsethsalazar <salazarjohnseth@gmail.com>
2024-06-13 10:06:05 +0800

    Added a tab widget to the Campaignes page with a Test component for it

--- app/routes/app.campaigns.tsx
+++ app/routes/app.campaigns.tsx
@@ -1,9 +1,22 @@
 import { Button, InlineGrid, Layout, LegacyCard, Page, Tabs, Text } from '@shopify/polaris'
-import React from 'react'
+import React, { useCallback, useState } from 'react'
+import Test from '~/components/Test'
 
 type Props = {}
 
 const CampaignsPage = (props: Props) => {
+  const [selected, setSelected] = useState(0)
+
+  const handleTabChange = useCallback((selectedTabIndex: number) => setSelected(selectedTabIndex), [])
+
+  const tabs = [
+    {
+      id: 'all-customers',
+      content: "All",
+      panelID: "all-customers-content-one",
+      component: <Test></Test>
+    }
+  ]
   return (
     <Page>
       <Layout>
@@ -12,6 +25,15 @@ const CampaignsPage = (props: Props) => {
             <Text variant='heading3xl' as='h2'>Campaigns</Text>
             <Button>Create New</Button>
           </InlineGrid>
+        </Layout.Section>
+        <Layout.Section>
+          <LegacyCard>
+            <Tabs tabs={tabs} selected={selected} onSelect={handleTabChange}>
+              <LegacyCard.Section title={tabs[selected].content}>
+                {tabs[selected].component}
+              </LegacyCard.Section>
+            </Tabs>
+          </LegacyCard>
         </Layout.Section>
       </Layout>
     </Page>
