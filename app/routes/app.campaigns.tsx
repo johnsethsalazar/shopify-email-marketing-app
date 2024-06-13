3fb859b9afdbe237c761298cc00b6a71e99a32d3
johnsethsalazar <salazarjohnseth@gmail.com>
2024-06-13 09:01:40 +0800

    Added basic elements to the campaigns tab

--- app/routes/app.campaigns.tsx
+++ app/routes/app.campaigns.tsx
@@ -1,10 +1,20 @@
+import { Button, InlineGrid, Layout, LegacyCard, Page, Tabs, Text } from '@shopify/polaris'
 import React from 'react'
 
 type Props = {}
 
 const CampaignsPage = (props: Props) => {
   return (
-    <div>CampaignsPage</div>
+    <Page>
+      <Layout>
+        <Layout.Section>
+          <InlineGrid columns={2}>
+            <Text variant='heading3xl' as='h2'>Campaigns</Text>
+            <Button>Create New</Button>
+          </InlineGrid>
+        </Layout.Section>
+      </Layout>
+    </Page>
   )
 }
 
